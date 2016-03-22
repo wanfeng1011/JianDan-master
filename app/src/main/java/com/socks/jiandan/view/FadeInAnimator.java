1b4f9f8deecabf0d0756eb140d71caca44ce234c
jiaxiangxiang <e912580949@qq.com>
2016-03-22 09:11:43 +0800

    Initial commit

---
+++ app/src/main/java/com/socks/jiandan/view/FadeInAnimator.java
@@ -0,0 +1,61 @@
+package com.socks.jiandan.view;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zhaokaiqiang on 15/4/24.
+ */
+public class FadeInAnimator extends RecyclerView.ItemAnimator{
+
+	private ArrayList<RecyclerView.ViewHolder> viewHolders;
+
+	public FadeInAnimator(){
+		viewHolders = new ArrayList<>();
+	}
+
+	@Override
+	public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, ItemHolderInfo postLayoutInfo) {
+		return false;
+	}
+
+	@Override
+	public boolean animateAppearance(@NonNull RecyclerView.ViewHolder viewHolder, ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+		return false;
+	}
+
+	@Override
+	public boolean animatePersistence(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+		return false;
+	}
+
+
+	@Override
+	public void runPendingAnimations() {
+
+	}
+
+	@Override
+	public void endAnimation(RecyclerView.ViewHolder item) {
+
+	}
+
+	@Override
+	public void endAnimations() {
+
+	}
+
+	@Override
+	public boolean isRunning() {
+		return false;
+	}
+
+	@Override
+	public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder,
+										  @NonNull RecyclerView.ViewHolder newHolder,
+										  @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo){
+		return false;
+	}
+}
